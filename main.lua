aff7386e2ff916a9c95c53fa8ba120daefb10239
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-04-04 11:06:32 -0400

    Change

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@
 --Hiding Status Bar
 display.setStatusBar(display.HiddenStatusBar)
 
-local automatic = math.random(1, 2)
+local automatic
 
 --Create the background
 local bck = display.setDefault("background", 247/255, 202/255, 201/255)
@@ -21,6 +21,9 @@ local iceCream = display.newImage("Images/icecream.png", 0, 0)
 
    --Setting Visibility
    iceCream.isVisible = false
+
+   --set the scale
+   iceCream:scale(0.5, 0.5)
 
 --Creating cupcake
 local cupcake = display.newImage("Images/cupcake.png", 0, 0)
@@ -37,6 +40,7 @@ local cupcake = display.newImage("Images/cupcake.png", 0, 0)
 
 --Score
 local cupcakeWhacked = 0
+local iceWhacked = 0
 
 --Score text
 local score = display.newText( "Score:"..cupcakeWhacked, 0, 0, "Georgia", 50)
@@ -48,22 +52,37 @@ local score = display.newText( "Score:"..cupcakeWhacked, 0, 0, "Georgia", 50)
    --Changing Color of Score
    score:setFillColor( 1, 0, 0)
 
+
+
 ---------------------------------------------------------------------------------------------
 --FUNCTIONS
 ---------------------------------------------------------------------------------------------------
 
 --This function makes the cupcake appear in a random (x, y) position
 function PopCake()
+  automatic = math.random(1, 2)
+  if (automatic == 1 ) then
+	  --choosing random possition
+	  cupcake.x = math.random( 0, display.contentWidth)
+	  cupcake.y = math.random( 0, display.contentHeight)
 
-	--choosing random possition
-	cupcake.x = math.random( 0, display.contentWidth)
-	cupcake.y = math.random( 0, display.contentHeight)
-
-	--Make the cupcake visible
+	  --Make the cupcake visible
     cupcake.isVisible = true
 
     --after 500 miliseconds, call the Hide function
-    timer.performWithDelay( 800, Hide)
+    timer.performWithDelay( 700, Hide)
+ 
+ elseif (automatic == 2 ) then
+  --choosing random possition
+  iceCream.x = math.random( 0, display.contentWidth)
+  iceCream.y = math.random( 0, display.contentHeight)
+
+  --Make the cupcake visible
+    iceCream.isVisible = true
+
+    --after 500 miliseconds, call the Hide function
+    timer.performWithDelay( 700, Hide)
+  end
 end
 
 --This function calls the PopDelay
@@ -76,9 +95,10 @@ function Hide( )
 
 	--Changing Visibility
 	cupcake.isVisible = false
+  iceCream.isVisible = false
 
 	--Staring the cycle over and over again
-	PopDelay()
+	PopDelayCake()
 end
 
 --This function starts the game
@@ -91,13 +111,26 @@ function Whacked( event )
 	--If touch phase just started
 	if (event.phase == "began") then
 		cupcakeWhacked = cupcakeWhacked + 1
+    print("cupcakeWhacked"..cupcakeWhacked)
 
 		score.text = "Score:" .. cupcakeWhacked
 	end
 end
 
+function Whacked2( event )
+
+  --If touch phase just started
+  if (event.phase == "began") then
+    cupcakeWhacked = cupcakeWhacked - 1
+    print("cupcakeWhacked"..cupcakeWhacked)
+
+    score.text = "Score:" .. cupcakeWhacked
+  end
+end
 
 --Event listener
-cupcake:addEventListener( 'touch', Whacked)
+cupcake:addEventListener( "touch", Whacked)
+iceCream:addEventListener("touch", Whacked2)
+
 
 GameStart()
